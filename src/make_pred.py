ed3c575c1c531a21faeeef6b5a1469585afe49c4
sanrajmitra97 <sanraj.mitra@gmail.com>
2022-10-28 22:06:07 +0800

    update

--- src/make_pred.py
+++ src/make_pred.py
@@ -1,9 +1,26 @@
-import pandas as pd
 import pickle
 from os import listdir, mkdir
 from os.path import isfile, join, exists
 from prep_data import preprocess
+import rich.progress
+import json
 
+outdir = '../results'
+cols_to_use = ['mean_lsd', 'mean_lm', 'var_lm', 'mean_csd', 'var_csd', 'mean_cm',
+    'var_cm', 'mean_rsd', 'mean_rm', 'var_rm', 'num_reads', 'G_count',
+    'T_count', 'relative_pos', 'order_1_A', 'order_1_T', 'order_2_A',
+    'order_2_G', 'order_3_A', 'order_4_A', 'order_4_C', 'order_4_T',
+    'order_5_A', 'order_5_G', 'order_5_T'] #xgb columns
+
+
+def make_predictions(data, model):
+    df_submit = data.loc[:, ['transcript_id', 'transcript_position']]
+    df_test = data[cols_to_use]
+    score = model.predict_proba(df_test.values)[:, 1]
+    df_submit['score'] = score
+    df_submit['score'] = df_submit['score'].astype(float)
+    #df_submit = df_submit.rename(columns={'curr_pos':'transcript_position'})
+    return df_submit
 
 ### Load Model
 print('LOADING MODEL')
@@ -14,45 +31,24 @@ print('##################LOADING MODEL COMPLETED##################')
 
 
 ### Load Data
-print('LOADING DATA')
 data_path = '../data/' #Update this line with dataset folder path
 data_names = [k for k in listdir(data_path) if isfile(join(data_path, k))]
 n_data = len(data_names)
-data_frames = []
 for i in range(n_data):
     # Send each json file for preprocessing. Get back a dataframe.
-    data = pd.read_json(data_path+data_names[i], lines=True)
-    prep = preprocess(data)
+    print('LOADING DATA')
+    with rich.progress.open(data_path+data_names[i], "rb") as file:
+        json_data = [json.loads(line) for line in file]
+    prep = preprocess(json_data)
     prep_df = prep.get_dataframe()
-    data_frames.append(prep_df)
-print('##################LOADING DATA COMPLETED##################')
-
-
-
-### Make predictions 
-print('MAKING PREDICTIONS')
-
-cols_to_use = ['mean_lsd', 'mean_lm', 'var_lm', 'mean_csd', 'var_csd', 'mean_cm',
-    'var_cm', 'mean_rsd', 'mean_rm', 'var_rm', 'num_reads', 'G_count',
-    'T_count', 'relative_pos', 'order_1_A', 'order_1_T', 'order_2_A',
-    'order_2_G', 'order_3_A', 'order_4_A', 'order_4_C', 'order_4_T',
-    'order_5_A', 'order_5_G', 'order_5_T'] #xgb columns
-
-
-def make_predictions(data, model):
-    df_submit = data.loc[:, ['transcript_id', 'curr_pos']]
-    df_test = data[cols_to_use]
-    score = model.predict_proba(df_test.values)[:, 1]
-    df_submit['score'] = score
-    df_submit['score'] = df_submit['score'].astype(float)
-    df_submit = df_submit.rename(columns={'curr_pos':'transcript_position'})
-    return df_submit
-
-outdir = '../results'
-for j in range(len(data_frames)):
-    results = make_predictions(data_frames[j], model)
-    outname = f'result_{j}.csv'
+    print('##################LOADING DATA COMPLETED##################')
+    ### Make predictions 
+    print('MAKING PREDICTIONS')
+    results = make_predictions(prep_df, model)
+    outname = f'result_{i}.csv'
     if not exists(outdir):
         mkdir(outdir)
     results.to_csv(f"{outdir}/{outname}", index=False)
-print('##################MAKING PREDICTIONS COMPLETED##################')
+    print('##################MAKING PREDICTIONS COMPLETED##################')
+
+
--- src/prep_data.py
+++ src/prep_data.py
@@ -1,100 +1,45 @@
 import pandas as pd
 import numpy as np
+from tqdm import tqdm
 
 class preprocess():
     def __init__(self, data):
-        self.data = data 
+        self.data = data
     def get_dataframe(self):
-        transcripts = self.data.columns
-        dataframes = []
-        for transcript in transcripts:
-            df_curr_transcript = self.data.loc[:, transcript]
-            df_curr_transcript.dropna(inplace=True)
-            df_curr_transcript = list(df_curr_transcript)
-            
-            for position in df_curr_transcript:
-                #position here is a dictionary
-                for k1, v1, in position.items():
-                    order = list(v1.keys())[0]
-                    for k2, v2 in v1.items():
+        all_rows = []
+        for list_index in tqdm(range(len(self.data))):
+            for gene_id in self.data[list_index]:
+                for pos in self.data[list_index][gene_id]:
+                    for bases in self.data[list_index][gene_id][pos]:
+                        reads = self.data[list_index][gene_id][pos][bases]
+                        avg_reads = [np.mean(x) for x in zip(*reads)]
+                        variance_reads = [np.var(x) for x in zip(*reads)]
+                        n_reads = [len(reads)]
+                        one_row = [gene_id, int(pos), bases] + avg_reads + variance_reads + n_reads
+                        all_rows.append(one_row)
 
-                        
-                        n = len(v2)
+        df = pd.DataFrame(all_rows, columns =['transcript_id', 'transcript_position', 'bases', 
+        'mean_ldt', 'mean_lsd', 'mean_lm', 
+        'mean_cdt', 'mean_csd', 'mean_cm',
+        'mean_rdt', 'mean_rsd', 'mean_rm',
+        'var_ldt', 'var_lsd', 'var_lm', 
+        'var_cdt', 'var_csd', 'var_cm',
+        'var_rdt', 'var_rsd', 'var_rm', 
+        'num_reads'])
 
-                        left_dwelling_t = np.zeros(n)
-                        left_sd = np.zeros(n)
-                        left_mean = np.zeros(n)
 
-                        centre_dwelling_t = np.zeros(n)
-                        centre_sd = np.zeros(n)
-                        centre_mean = np.zeros(n)
+        df["order_1"] = df["bases"].apply(lambda x: x[0])
+        df["order_2"] = df["bases"].apply(lambda x: x[1])
+        df["order_3"] = df["bases"].apply(lambda x: x[2])
+        df["order_4"] = df["bases"].apply(lambda x: x[5])
+        df["order_5"] = df["bases"].apply(lambda x: x[6])
+        df["A_count"] = df["bases"].apply(lambda x: x.count('A'))
+        df["G_count"] = df["bases"].apply(lambda x: x.count('G'))
+        df["C_count"] = df["bases"].apply(lambda x: x.count('C'))
+        df["T_count"] = df["bases"].apply(lambda x: x.count('T'))
+        categorical_cols = ['order_1', 'order_2', 'order_3', 'order_4', 'order_5']
 
-                        right_dwelling_t = np.zeros(n)
-                        right_sd = np.zeros(n)
-                        right_mean = np.zeros(n)
-
-                        for i in range(n):
-                            left_dwelling_t[i] = v2[i][0]
-                            left_sd[i] = v2[i][1]
-                            left_mean[i] = v2[i][2]
-
-                            centre_dwelling_t[i] = v2[i][3]
-                            centre_sd[i] = v2[i][4]
-                            centre_mean[i] = v2[i][5]
-                            
-                            right_dwelling_t[i] = v2[i][6]
-                            right_sd[i] = v2[i][7]
-                            right_mean[i] = v2[i][8]
-
-                        left_dwelling_t_mean = np.mean(left_dwelling_t)
-                        left_dwelling_t_var = np.var(left_dwelling_t)
-                        left_sd_mean = np.mean(left_sd)
-                        left_sd_var = np.var(left_sd)
-                        left_mean_mean = np.mean(left_mean)
-                        left_mean_var = np.var(left_mean)
-
-                        centre_dwelling_t_mean = np.mean(centre_dwelling_t)
-                        centre_dwelling_t_var = np.var(centre_dwelling_t)
-                        centre_sd_mean = np.mean(centre_sd)
-                        centre_sd_var = np.var(centre_sd)
-                        centre_mean_mean = np.mean(centre_mean)
-                        centre_mean_var = np.var(centre_mean)
-                        
-                        right_dwelling_t_mean = np.mean(right_dwelling_t)
-                        right_dwelling_t_var = np.var(right_dwelling_t)
-                        right_sd_mean = np.mean(right_sd)
-                        right_sd_var = np.var(right_sd)
-                        right_mean_mean = np.mean(right_mean)
-                        right_mean_var = np.var(right_mean)
-
-                        
-                        
-                    final_order = order[:3] + order[5:]
-                    curr_dataframe = pd.DataFrame({'transcript_id':[transcript], 'order': [final_order], 'curr_pos': [k1], 
-                                                'mean_ldt': [left_dwelling_t_mean], 'var_ldt':[left_dwelling_t_var], 'mean_lsd': [left_sd_mean], 'var_lsd': [left_sd_var], 'mean_lm':[left_mean_mean], 'var_lm': [left_mean_var], 
-                                                'mean_cdt': [centre_dwelling_t_mean], 'var_cdt':[centre_dwelling_t_var] , 'mean_csd':[centre_sd_mean], 'var_csd': [centre_sd_var], 'mean_cm': [centre_mean_mean], 'var_cm': [centre_mean_var],  
-                                                'mean_rdt': [right_dwelling_t_mean], 'var_rdt':[right_dwelling_t_var], 'mean_rsd': [right_sd_mean], 'var_rsd':[right_sd_var], 'mean_rm': [right_mean_mean], 'var_rm':[right_mean_var], 
-                                                'num_reads': n})   
-                    dataframes.append(curr_dataframe)
-        final_df = pd.concat(dataframes)
-        final_df['curr_pos'] = final_df['curr_pos'].astype('int64')
-        final_df['A_count'] = final_df['order'].apply(lambda x: x.count('A'))
-        final_df['C_count'] = final_df['order'].apply(lambda x: x.count('C'))
-        final_df['G_count'] = final_df['order'].apply(lambda x: x.count('G'))
-        final_df['T_count'] = final_df['order'].apply(lambda x: x.count('T'))
-        final_df['relative_pos'] = final_df.groupby('transcript_id')['curr_pos'].rank()
-        final_df['relative_pos'] = final_df['relative_pos'].astype(int)
-        final_df['order_1'] = final_df['order'].apply(lambda x: x[0])
-        final_df['order_2'] = final_df['order'].apply(lambda x: x[1])
-        final_df['order_3'] = final_df['order'].apply(lambda x: x[2])
-        final_df['order_4'] = final_df['order'].apply(lambda x: x[3])
-        final_df['order_5'] = final_df['order'].apply(lambda x: x[4])
-        
-        #categorical data
-        categorical_cols = ['order_1', 'order_2', 'order_3',
-            'order_4', 'order_5']
-
-        #import pandas as pd
-        final_df = pd.get_dummies(final_df, columns = categorical_cols)
-        return final_df 
-
+        df = pd.get_dummies(df, columns = categorical_cols)
+        df["relative_pos"] = df.groupby("transcript_id")["transcript_position"].rank()
+        df["relative_pos"] = df["relative_pos"].astype(int)
+        return df
